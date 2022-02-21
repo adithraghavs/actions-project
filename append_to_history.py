c9b9bfda0ea3154a9d4049e33572c5a0ec3d5cf1
Adith Raghav <adithraghavs@gmail.com>
2022-02-21 11:29:28 +0530

    fixed append_to_history

--- append_to_history.py
+++ append_to_history.py
@@ -1,5 +1,12 @@
-import json
-history = open("history.json")
+import json, subprocess
+
+history = open("history.json", "r")
 history_dict = json.loads(history.read())
 
 data = open("data.xml").read()
+hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode()[:-2]
+history_dict[hash] = data
+history_json = json.dumps(history_dict, indent=4)
+
+history = open("history.json", "w")
+history.write(history_json)
