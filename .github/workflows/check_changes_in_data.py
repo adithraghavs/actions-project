6bfadbbea9bfd87c66984f6a43271f09684e0969
Adith Raghav <adithraghavs@gmail.com>
2022-02-21 13:13:20 +0530

    done

---
+++ .github/workflows/check_changes_in_data.py
@@ -0,0 +1,6 @@
+import json, re
+data = open("data.xml").read()
+history = json.loads(open("history.json", "r").read())
+if history[list(history.keys())[-1]] != data:
+    with open("main.txt", "w") as file:
+        file.write(data.split('<color name="')[1].split('">Ipsum<color>')[0])
